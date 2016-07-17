5467466b3d10cab04097c88ca2d401f75152f785
Emilio Gomez Lavin <emiliogomezlavin@gmail.com>
2016-07-16 20:09:58 -0700

    adds changes to prueba worker

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 
 	def self.prueba()
-		puts "hola!"
+		User.create({
+          first_name: "Santi",
+          last_name: "Me",
+          email: "ayudo@worker.com",
+          password_digest: "yeiii"
+          })
 	end
 end
